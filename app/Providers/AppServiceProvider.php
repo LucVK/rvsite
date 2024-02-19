41aaa7f2181ece53a5b1475bd4424e0fee40f6b8
Luc Van Keer <luc.vankeer@telenet.be>
2024-02-19 18:54:39 +0100

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Statamic\Statamic;
+use App\Providers\MailerSend\MailerSendBulkTransport;
+use MailerSend\MailerSend;
+use Illuminate\Support\Arr;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Mail\MailManager;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(MailerSend::class, function(Application $app) {
+            $config = $this->app['config']->get('mailersend-driver', []);
+
+            return new MailerSend([
+                'api_key' => Arr::get($config, 'api_key'),
+                'host' => Arr::get($config, 'host'),
+                'protocol' => Arr::get($config, 'protocol'),
+                'api_path' => Arr::get($config, 'api_path'),
+            ]);
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Statamic::vite('app', [
+        //     'resources/js/cp.js',
+        //     'resources/css/cp.css',
+        // ]);
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        $this->app->make(MailManager::class)->extend('mailersendbulk', function () {
+            $config = $this->app['config']->get('mailersend-driver', []);
+
+            $mailersend = new MailerSend([
+                'api_key' => Arr::get($config, 'api_key'),
+                'host' => Arr::get($config, 'host'),
+                'protocol' => Arr::get($config, 'protocol'),
+                'api_path' => Arr::get($config, 'api_path'),
+            ]);
+
+            return new MailerSendBulkTransport($mailersend);
+        });            
+
+        $this->loadJsonTranslationsFrom(base_path() . "/lang/rvwaarloos");
+
+    }
+}
