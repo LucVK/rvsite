4eaa615cd0e173a1e85ec952f59df10bfae13f56
Luc Van Keer <luc.vankeer@telenet.be>
2024-02-20 12:04:39 +0100

    saving

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,13 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Studio1902\PeakSeo\Handlers\ErrorPage;
+use Statamic\Statamic;
+use App\Providers\MailerSend\MailerSendBulkTransport;
+use MailerSend\MailerSend;
+use Illuminate\Support\Arr;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Mail\MailManager;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +19,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(MailerSend::class, function(Application $app) {
+            $config = $this->app['config']->get('mailersend-driver', []);
+
+            return new MailerSend([
+                'api_key' => Arr::get($config, 'api_key'),
+                'host' => Arr::get($config, 'host'),
+                'protocol' => Arr::get($config, 'protocol'),
+                'api_path' => Arr::get($config, 'api_path'),
+            ]);
+        });
     }
 
     /**
@@ -24,5 +40,25 @@ class AppServiceProvider extends ServiceProvider
         // Statamic::style('app', 'cp');
 
         ErrorPage::handle404AsEntry();
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        $this->app->make(MailManager::class)->extend('mailersendbulk', function () {
+            $config = $this->app['config']->get('mailersend-driver', []);
+
+            $mailersend = new MailerSend([
+                'api_key' => Arr::get($config, 'api_key'),
+                'host' => Arr::get($config, 'host'),
+                'protocol' => Arr::get($config, 'protocol'),
+                'api_path' => Arr::get($config, 'api_path'),
+            ]);
+
+            return new MailerSendBulkTransport($mailersend);
+        });            
+
+        $this->loadJsonTranslationsFrom(base_path() . "/lang/rvwaarloos");
+
     }
 }
---
+++ content/collections/pages/clubmembers.md
@@ -0,0 +1,10 @@
+---
+id: 25326a4a-6a55-4abb-a676-fa3bc2db9c19
+blueprint: page
+title: Clubmembers
+template: clubmembers/index
+page_builder:
+  -
+    type: index_content
+    enabled: 'true'
+---
